1dc86611a7e73f081454adba071d185543178dbf
gus <gustavopantoja.ap@gmail.com>
2022-10-19 18:11:46 -0300

    agora vai

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,20 +12,7 @@
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('/apple-icon-57x57.png') }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('/apple-icon-60x60.png') }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/apple-icon-72x72.png') }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('/apple-icon-76x76.png') }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/apple-icon-114x114.png') }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('/apple-icon-120x120.png') }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('/apple-icon-144x144.png') }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('/apple-icon-152x152.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('/android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/favicon-96x96.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('/manifest.json') }}">
+
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="{{ asset('/ms-icon-144x144.png') }}">
     <meta name="theme-color" content="#ffffff">
@@ -130,78 +117,72 @@ webkit-background-size: cover;
 background-size: cover;
 height: 100%;
 width: 100%;">
-    <div class="container">
+<div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <div class="col-md-12" style="margin-top: 45px;margin-bottom: 35px">
-                <center>
-                    <img width="300x" class="img img-responsive" src="{{ asset(url('logo.png')) }}" alt="">
-                </center>
-
-
+        <div class="col-md-12" style="margin-top: 45px;margin-bottom: 35px">
+            <center>
+                <img width="300x" class="img img-responsive" src="{{ asset(url('logo.png')) }}" alt="">
+            </center>
 
 
-            </div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4 caixa">
-                <form class="m-t" method="POST" action="{{ route('login') }}">
-                    @csrf
-
-                    <!-- Email Address -->
-                    <div class="form-group">
-                        <input placeholder="LOGIN" class="form-control inputfundo" id="login" type="text" name="login"
-                               value="{{ old('login') }}" required />
-                    </div>
-
-                    <!-- Password -->
-                    <div class="form-group">
-
-                        <input placeholder="PASSWORD" class="form-control boleado input-lg inputfundo" id="password"
-                            type="password" name="password" required />
-                    </div>
-
-                    <!-- Remember Me -->
-                    <div class="row text-center">
-                        <div style="margin-top: 10px" class="col-md-6">
-                            <button type="submit" class="btn intas">Login
-                            </button>
-                        </div>
-                        <div style="margin-top: 10px" class="col-md-6">
-                            <a class="btn intas" href="{{ url('register') }}">REGISTER
-                            </a>
-                        </div>
-                    </div>
-                    <div class="row text-center">
-                        <div style="margin-top: 10px" class="col-md-12">
-                            <a href="{{ route('password.request') }}" class="btn intas">Forget Password
-                            </a>
-                        </div>
-
-                    </div>
-                    <br>
-                    <br>
-
-
-
-
-
-                </form>
-            </div>
-            <div class="col-md-4"></div>
         </div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4 caixa">
+            <form id="myForm" class="m-t" method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <!-- Email Address -->
+                <div class="form-group">
+                    <input placeholder="LOGIN" class="form-control inputfundo" id="login" type="text" name="login"
+                           value="{{ old('login') }}" required/>
+                </div>
+
+                <!-- Password -->
+                <div class="form-group">
+
+                    <input placeholder="PASSWORD" class="form-control boleado input-lg inputfundo" id="password"
+                           type="password" name="password" required/>
+                </div>
+
+                <!-- Remember Me -->
+                <div class="row text-center">
+                    <div style="margin-top: 10px" class="col-md-6">
+                        <button id="submitBtn" type="submit" class="btn intas">Login
+                        </button>
+                    </div>
+                    <div style="margin-top: 10px" class="col-md-6">
+                        <a class="btn intas" href="{{ url('register') }}">REGISTER
+                        </a>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <div style="margin-top: 10px" class="col-md-12">
+                        <a href="{{ route('password.request') }}" class="btn intas">Forget Password
+                        </a>
+                    </div>
+
+                </div>
+                <br>
+                <br>
+
+
+            </form>
+        </div>
+        <div class="col-md-4"></div>
     </div>
+</div>
 </body>
 
-<script src="{{ asset('admin/bower_components/jquery/dist/jquery.min.js') }}" >
-
-
-<script type="text/javascript">
-    function googleTranslateElementInit() {
-        new google.translate.TranslateElement({
-            pageLanguage: 'en'
-        }, 'google_translate_element');
-    }
+<script src="{{ asset('admin/bower_components/jquery/dist/jquery.min.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $("#submitBtn").click(function () {
+            $('#submitBtn').attr('disabled', 'disabled');
+            $("#myForm").submit(); // Submit the form
+        });
+    });
 </script>
 
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
