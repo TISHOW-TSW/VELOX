15563dd29f23bf08764e3e58147c5ba4d4568b50
gus <gustavopantoja.ap@gmail.com>
2022-11-03 10:15:35 -0300

    dia dos finados

--- resources/views/cliente/saque/rendimento.blade.php
+++ resources/views/cliente/saque/rendimento.blade.php
@@ -165,7 +165,16 @@
                                     <a href="{{ url('/myaccount') }}" class="btn btn-success btn-block text-uppercase">Cadastrar Meio saque</a>
                                 @else
 
-                                    <label for="">Saque de Rendimento sera a partir de Quinta-feira</label>
+                                    @if($agora>= '09:00:00'&&$agora<='18:00:00')
+                                        <button type="submit" name="submit"
+                                                class="btn btn-success btn-block text-uppercase">Solicitar saque</button>
+                                    @else
+
+                                        <label for="">Saques pertidos nos horarios entre 9H as 18H</label>
+
+
+                                    @endif
+
 
                                 @endif
 
